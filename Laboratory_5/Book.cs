4074a9a35be375a5e059f56459535c9324761bb1
liza <liza@DESKTOP-OST2SGM>
2024-11-18 01:25:30 +0200

    Add project files.

---
+++ Laboratory_5/Book.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory_5
+{
+	public class Book
+	{
+		public string Title { get; set; }
+		public string Author { get; set; }
+		public int NumPages { get; set; }
+		public int WordCount { get; set; }
+		public string Publisher { get; set; }
+		public int YearPublished { get; set; }
+		public string Language { get; set; }
+		public string Genre { get; set; }
+		public int Onthepage { get; set; }
+		public bool SizeofBook {  get; set; } 
+
+
+		public int CalculateWordsPerPage()
+		{
+			if (NumPages > 0)
+			{
+				return (int)WordCount / NumPages;
+			}
+			else
+			{
+				Console.WriteLine("Кількість сторінок не може бути 0 або менше.");
+				return 0;
+			}
+		}
+
+		public bool IsLargeBook()
+		{
+			if (NumPages >= 500)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+
+		public double WordsPerPage
+		{
+			get { return CalculateWordsPerPage(); }
+		}
+
+
+
+		public bool BigorSmall
+		{
+			get
+			{
+				return NumPages >= 500;
+			}
+		}
+
+
+	
+		public Book() { }
+
+
+		public Book(string title, string author, int numPages, int wordCount, string publisher, int yearPublished, 
+			string language, string genre, int onthepage, bool sizeofbook)
+		{
+			Title = title;
+			Author = author;
+			NumPages = numPages;
+			WordCount = wordCount;
+			Publisher = publisher;
+			YearPublished = yearPublished;
+			Language = language;
+			Genre = genre;
+			Onthepage = onthepage; 
+			SizeofBook = sizeofbook;
+		}
+	}
+}
---
+++ Laboratory_5/fBook.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratory_5
+{
+	public partial class fBook : Form
+	{
+		public fBook(Book t)
+		{
+			TheBook = t;
+			InitializeComponent();
+		}
+
+		public Book TheBook;
+
+		private void fBook_Load(object sender, EventArgs e)
+		{
+			if (TheBook != null)
+			{
+				tbName.Text = TheBook.Title;
+				tbAuthor.Text = TheBook.Author;
+				tbLanguage.Text = TheBook.Language;
+				tbYearPublished.Text = TheBook.YearPublished.ToString();
+				tbGenre.Text = TheBook.Genre;
+				tbPublisher.Text = TheBook.Publisher;
+				tbNumPages.Text = TheBook.NumPages.ToString();
+				tbWordCount.Text = TheBook.WordCount.ToString();
+			}
+
+
+		}
+
+		private void btnOk_Click(object sender, EventArgs e)
+		{
+			TheBook.Title = tbName.Text.Trim();
+			TheBook.Author = tbAuthor.Text.Trim();
+			TheBook.Language = tbLanguage.Text.Trim();
+			TheBook.YearPublished = int.Parse(tbYearPublished.Text.Trim());
+			TheBook.Genre = tbGenre.Text.Trim();
+			TheBook.Publisher = tbPublisher.Text.Trim();
+			TheBook.NumPages = int.Parse(tbNumPages.Text.Trim());
+			TheBook.WordCount = int.Parse(tbWordCount.Text.Trim());
+
+			DialogResult = DialogResult.OK;
+		}
+
+		private void btnCancel_Click(object sender, EventArgs e)
+		{
+			DialogResult = DialogResult.Cancel;
+		}
+
+		
+	}
+}
